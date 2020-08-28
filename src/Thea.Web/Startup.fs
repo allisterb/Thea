04ccabd01e7c3e830c06d3a9699de949c7dd3867
Allister Beharry <allister.beharry@gmail.com>
2020-08-28 16:24:41 -0400

    Add Thea.Web project with ext referemces, builds and runs under WebSharper libs 4.5.x

---
+++ src/Thea.Web/Startup.fs
@@ -0,0 +1,36 @@
+namespace Thea.Web
+
+open System
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open WebSharper.AspNetCore
+
+type Startup() =
+
+    member this.ConfigureServices(services: IServiceCollection) =
+        ()
+
+    member this.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
+        if env.IsDevelopment() then app.UseDeveloperExceptionPage() |> ignore
+
+        app.UseDefaultFiles()
+            .UseStaticFiles()
+            .Run(fun context ->
+                        context.Response.StatusCode <- 404
+                        context.Response.WriteAsync("Page not found"))
+
+module Program =
+
+    [<EntryPoint>]
+    let main args =
+        WebHost
+            .CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build()
+            .Run()
+        0
